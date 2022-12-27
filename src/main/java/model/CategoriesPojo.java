23f7213a38e0c0b71e2b25943abb4c41d02bc3e6
ajay210677 <114820983+ajay210677@users.noreply.github.com>
2022-12-27 20:35:14 +0000

    Initial commit

---
+++ src/main/java/model/CategoriesPojo.java
@@ -0,0 +1,17 @@
+package model;
+
+public class CategoriesPojo {
+    private String name;
+    private String id;
+
+    public String getName(){
+        return name;
+
+    }
+    public void setName(String name){
+        this.name =name;
+    }
+    public String getId(){
+        return id;
+    }
+}
---
+++ src/main/java/model/ServicePojo.java
@@ -0,0 +1,5 @@
+package model;
+
+public class ServicePojo {
+    private String name;
+}
